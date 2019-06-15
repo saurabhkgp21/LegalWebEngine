93eb1b80d57455f22116e4da15d7b0c8e4fd573a
Saurabh Agarwal <shourabh.agarwal@gmail.com>
2019-06-15 14:23:07 +0530

    multiselect choices working

--- input/forms.py
+++ input/forms.py
@@ -2,9 +2,25 @@ from django import forms
 
 
 class InputForm(forms.Form):
-    srch = forms.CharField(label='Search', max_length=10000)
-    dateFrom = forms.DateField(label='DateFrom')
-    dateTo = forms.DateField(label='dateTo')
-    states1 = forms.ChoiceField(label='Category')
-    states2 = forms.ChoiceField(label='Acts')
-    states3 = forms.ChoiceField(label='Judge')
+	naturalLanguageQuery = forms.CharField(label='Search', max_length=10000)
+	dateFrom = forms.DateField(label='DateFrom', required=False)
+	dateTo = forms.DateField(label='dateTo', required=False)
+
+	CATEGORIES = (
+		('a', 'A'),
+		('b', 'B')
+	)
+
+	ACTS = (
+		('c', 'C'),
+		('d', 'D')
+	)
+
+	JUDGES = (
+		('e', 'E'),
+		('f', 'F')
+	)
+
+	categories = forms.MultipleChoiceField(choices=CATEGORIES, widget=forms.CheckboxSelectMultiple(), label='Categories', required=False)
+	acts = forms.MultipleChoiceField(choices=ACTS, widget=forms.CheckboxSelectMultiple(), label='Acts', required=False)
+	judges = forms.MultipleChoiceField(choices=JUDGES, widget=forms.CheckboxSelectMultiple(), label='Judges', required=False)
--- input/views.py
+++ input/views.py
@@ -5,14 +5,18 @@ from .forms import InputForm
 
 
 def index(request):
-    return render(request, 'input/index.html')
+	form = InputForm()
+	return render(request, 'input/index.html', {'form': form})
 
 
 def output(request):
-    form = InputForm()
-    if request.method == 'POST':
-        form = InputForm(request.POST)
-        if form.is_valid():
-            #Call the python function
-            return render(request, 'input/output.html', {'form': form})
-    return render(request, 'input/output.html')
+	form = InputForm()
+	if request.method == 'POST':
+		print(request.POST)
+		form = InputForm(request.POST)
+		if form.is_valid():
+			#Call the python function
+			return render(request, 'input/output.html', {'form': form})
+		else:
+			print(form.errors)
+	return render(request, 'input/output.html')
